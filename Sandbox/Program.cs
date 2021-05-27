c78b7ab719d0399fdbdee027947f34b55c9c0d16
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-05-27 13:39:55 +0100

    Add Sandbox code
    Code taken from https://www.red-gate.com/simple-talk/dotnet/net-development/creating-ccli-wrapper/

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using CLI;
 namespace Sandbox
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Entity e = new Entity("The Wallman", 20, 35);
+            e.Move(5, -10);
+            Console.WriteLine(e.XPosition + " " + e.YPosition);
+            Console.Read();
         }
     }
 }
