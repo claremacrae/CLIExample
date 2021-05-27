8b724ea3049341904b6a3e86c9a0c11a5f76f925
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-05-27 16:32:22 +0100

    Add tests using .Net Framework - these can call CLI code!!!

---
+++ TestsNetFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestsNetFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestsNetFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c15997a-51c8-4f8f-a388-cdf56a295eb6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestsNetFramework/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using CLI;
+using Native;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTestNetFramework
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.AreEqual(4, 4);
+            Assert.AreEqual("Hello from C#", Class1.Hello());
+            Entity e = new Entity("The Wallman", 20, 35);
+            Assert.AreEqual(e.XPosition, 20);
+        }
+    }
+}
